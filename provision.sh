ae04e5240a5ca9986f4f2d7e4eb3ae50b602c68e
PandaJoey <up623013@myport.ac.uk>
2018-06-27 14:08:41 +0100

    14.08, 27/06/2018

--- provision.sh
+++ provision.sh
@@ -7,7 +7,50 @@ apt-get -y install nodejs
 apt-get -y install npm
 git clone https://github.com/PandaJoey/vagrantTestScript.git
 cd vagrantTestScript/
+ip="$(ifconfig | grep wlp -A 2 | grep inet  | awk '{match($0,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); ip = substr($0,RSTART,RLENGTH); print ip}')"
+#ip="$(ifconfig | grep enp -A 2 | grep inet  | awk '{match($0,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); ip = substr($0,RSTART,RLENGTH); print ip}')"
+echo "upstream app_Hello {
+        server $ip:3012;
+}
+server {
+        listen 80;
+        server_name l.hellovm.akerolabs.com;
+        access_log /var/log/nginx/hello-admin.log;
+        location / {
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header Host $http_host;
+                proxy_set_header X-NginX-Proxy true;
+                proxy_set_header Upgrade $https_upgrade;
+                proxy_set_header Connection "upgrade";
+                proxy_pass http://app_Hello/;
+                proxy_redirect off;
+                proxy_http_version 1.1;
+                proxy_buffering off;
+        }
+}" > hello-app
+echo "upstream app_HelloVm {
+        server $ip:3025;
+}
+server {
+        listen 80;
+        server_name l.hellovm.akerolabs.com;
+        access_log /var/log/nginx/hello-admin.log;
+        location / {
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header Host $http_host;
+                proxy_set_header X-NginX-Proxy true;
+                proxy_set_header Upgrade $https_upgrade;
+                proxy_set_header Connection "upgrade";
+                proxy_pass http://app_HelloVm/;
+                proxy_redirect off;
+                proxy_http_version 1.1;
+                proxy_buffering off;
+        }
+}" > hellovm-app
 mv hello-app /etc/nginx/sites-enabled/
+mv hellovm-app /etc/nginx/sites-enabled/
 sudo nginx -s stop
 sudo service nginx start
 npm install -y
