3a184f021702d045e04d83e3af1e4e83b681d384
PandaJoey <up623013@myport.ac.uk>
2018-06-28 12:21:31 +0100

    Update provision.sh

--- vagrantInitFiles/provision.sh
+++ vagrantInitFiles/provision.sh
@@ -46,7 +46,7 @@ echo 'server {
                 proxy_set_header X-NginX-Proxy true;
                 proxy_set_header Upgrade $http_upgrade;
                 proxy_set_header Connection "upgrade";
-                proxy_pass http://app_Hello/;
+                proxy_pass http://app_HelloVm/;
                 proxy_redirect off;
                 proxy_http_version 1.1;
                 proxy_buffering off;
